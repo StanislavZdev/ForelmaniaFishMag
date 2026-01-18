5f7247cfaa980a3bdc834a94caaa7dd7cd5c9184
StanislavZdev <stanislav.zrom1199@gmail.com>
2026-01-18 19:11:19 +0300

    First Spring Boot structure

---
+++ src/test/java/org/example/forelmaniafishmag/ForelmaniaFishMagApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.forelmaniafishmag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForelmaniaFishMagApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
