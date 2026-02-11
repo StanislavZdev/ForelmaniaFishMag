cbf5f6c52c56e8ea8da74e4067a727699bb147ce
StanislavZdev <stanislav.zrom1199@gmail.com>
2026-02-11 23:11:49 +0300

    add service code

--- src/main/java/org/example/forelmaniafishmag/service/ForelmaniaFishMagWebhookService.java
+++ src/main/java/org/example/forelmaniafishmag/service/ForelmaniaFishMagWebhookService.java
@@ -3,6 +3,11 @@ package org.example.forelmaniafishmag.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.example.forelmaniafishmag.repository.ClientRepository;
+import org.example.forelmaniafishmag.repository.ProductRepository;
+import org.example.forelmaniafishmag.repository.TildaWebhookDataRepository;
+import org.example.forelmaniafishmag.tildawebhook.dto.TildaWebhookRequestDTO;
+import org.example.forelmaniafishmag.tildawebhook.model.TildaWebhookData;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,4 +17,22 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 
 public class ForelmaniaFishMagWebhookService {
+
+    private final TildaWebhookDataRepository tildaWebhookDataRepository;
+    private final ProductRepository productRepository;
+    private final ClientRepository clientRepository;
+
+    @Transactional
+    public TildaWebhookData processWebhook(TildaWebhookRequestDTO dto) {
+        log.info("Processing Tilda webhook: {}", dto);
+
+        String name = dto.getName();
+        String email = dto.getEmail();
+        String phone = dto.getPhone();
+
+        if (name == null || phone == null || name.isBlank() || phone.isBlank()) {
+            throw new IllegalArgumentException("Name or Phone is required");
+        }
+
+    }
 }
